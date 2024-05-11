737dbb588b958d3a6bb70cf50fb6aa8e6adf61d9
notobo15 <binh15122003@gmail.com>
2024-05-12 00:40:42 +0700

    feat: save the analyst's image after training

--- CNN/Training.py
+++ CNN/Training.py
@@ -7,6 +7,8 @@ from keras.preprocessing.image import ImageDataGenerator
 import keras
 from keras.callbacks import ModelCheckpoint
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 class Training:
     def __init__(self, size=112, epochs=20, patch_sizes=64, optimizer='adam', loss='categorical_crossentropy', test_size=0.2, num_classes=0, folder_path='',PoolingType = 'AveragePooling'):
         self.model = None
@@ -19,6 +21,8 @@ class Training:
         self.test_size = test_size
         self.folder_path = folder_path
         self.PoolingType = PoolingType
+        self.history = None
+        self.folder_train_path = None
 
     def build_model(self):
         base_model = MobileNet(include_top=False, weights="imagenet", input_shape=(self.size,self.size,3))
@@ -76,7 +80,7 @@ class Training:
         #                         callbacks=callback_list,
         #                         epochs=self.epochs)
 
-        self.model.fit(train_generator, steps_per_epoch=len(train_generator),
+        self.history = self.model.fit(train_generator, steps_per_epoch=len(train_generator),
                validation_data=validation_generator, validation_steps=len(validation_generator),
                callbacks=callback_list, epochs=self.epochs)
 
@@ -87,3 +91,40 @@ class Training:
     def save_model(self):
         tfjs.converters.save_keras_model(self.model, self.folder_path)
         self.delete_model_checkpoint()
+
+        self.plot_training_history()
+
+
+    def plot_training_history(self):
+        # Lấy các giá trị loss và accuracy từ history
+        history = self.history
+        # epochs = self.epochs
+        train_loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        train_acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
+        epochs = range(1, len(train_loss) + 1)
+        epochs_val = range(1, len(val_loss) + 1)
+
+        # Vẽ đồ thị
+        plt.figure(figsize=(10, 5))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs, train_loss, label='Training Loss')
+        plt.plot(epochs_val, val_loss, label='Validation Loss')
+        plt.title('Training and Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs, train_acc, label='Training Accuracy')
+        plt.plot(epochs, val_acc, label='Validation Accuracy')
+        plt.title('Training and Validation Accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.legend()
+
+        plt.tight_layout()
+                    
+        if self.folder_path is not None:
+            plt.savefig(os.path.join(self.folder_path, 'analyst.png'))
+        
